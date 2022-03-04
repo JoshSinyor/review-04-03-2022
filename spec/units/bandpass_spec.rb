8c996be06f53c62321805ec5ecfd0f9b138ed947
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-04 16:15:29 +0000

    Update bandpass_spec.rb. Reflects refactoring in concert with Rubocop.

--- spec/units/bandpass_spec.rb
+++ spec/units/bandpass_spec.rb
@@ -3,24 +3,24 @@
 require 'bandpass'
 
 describe 'bandpass_filter' do
-  it "filters out frequencies that are lower or higher than the default limits" do
+  it 'filters out frequencies that are lower or higher than the default limits' do
     expect(bandpass_filter([30])).to eq [40]
     expect(bandpass_filter([1100])).to eq [1000]
   end
 
-  it "filters out multiple frequencies that are lower or higher than the default limits" do
-    expect(bandpass_filter([30,40,500,1000,1010])).to eq [40,40,500,1000,1000]
+  it 'filters out multiple frequencies that are lower or higher than the default limits' do
+    expect(bandpass_filter([30, 40, 500, 1000, 1010])).to eq [40, 40, 500, 1000, 1000]
   end
 
-  it "filters out multiple frequencies that are lower or higher than overridden limits" do
-    expect(bandpass_filter([50,100,500,1100,1150],100,1100)).to eq [100,100,500,1100,1100]
+  it 'filters out multiple frequencies that are lower or higher than overridden limits' do
+    expect(bandpass_filter([50, 100, 500, 1100, 1150], 100, 1100)).to eq [100, 100, 500, 1100, 1100]
   end
 
-  it "throws an error when not supplied with an array" do
-    expect{ bandpass_filter("Hello World!") }.to raise_error("Sound waves are not parsed correctly")
+  it 'throws an error when not supplied with an array' do
+    expect { bandpass_filter('Hello World!') }.to raise_error('Sound waves are not parsed correctly')
   end
 
-  it "passes the first acceptance criterion" do
-    expect(bandpass_filter([60,10,45,60,1500])).to eq [60,40,45,60,1000]
+  it 'passes the first acceptance criterion' do
+    expect(bandpass_filter([60, 10, 45, 60, 1500])).to eq [60, 40, 45, 60, 1000]
   end
 end
