bc02d2f6c595919d3695c32464bfc654c5aae3e0
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-04 15:32:07 +0000

    Update bandpass.rb and bandpass_spec.rb. Reflects passing of third round of tests.

--- lib/bandpass.rb
+++ lib/bandpass.rb
@@ -2,4 +2,5 @@
 
 def bandpass_filter(array)
   array.map! { |x| x < 40 ? x = 40 : x = x }
+  array.map! { |x| x > 1000 ? x = 1000 : x = x }
 end
--- spec/units/bandpass_spec.rb
+++ spec/units/bandpass_spec.rb
@@ -18,4 +18,12 @@ describe 'bandpass_filter' do
   it "filters out multiple frequencies that are too low" do
     expect(bandpass_filter([20,30,40,50,60])).to eq [40,40,40,50,60]
   end
+
+  it "filters out frequencies that are too high" do
+    expect(bandpass_filter([1010])).to eq [1000]
+  end
+
+  it "filters out multiple frequencies that are too low" do
+    expect(bandpass_filter([990,1000,1010,1020])).to eq [990,1000,1000,1000]
+  end
 end
