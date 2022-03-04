2126af169c2676829acc44d5dd459d6b137684f5
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-04 15:29:50 +0000

    Update bandpass.rb. Reflects refactoring.

--- lib/bandpass.rb
+++ lib/bandpass.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 def bandpass_filter(array)
-  array.map! do |x|
-    if x < 40
-      x = 40
-    else
-      x = x
-    end
-  end
+  array.map! { |x| x < 40 ? x = 40 : x = x }
 end
